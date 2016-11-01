3df217d62eba7675424c0bc35526e3e65ef7175b
Yulia Nastenko <yulia.nastenko@gmail.com>
2016-11-01 20:34:22 +0100

    Array Power

---
+++ arrayPow.js
@@ -0,0 +1,10 @@
+function arrayPow(array, power) {
+  var result = [];
+  for (i = 0; i < array.length; i++) {
+    result.push(Math.pow(array[i], power));
+  }
+  return result;
+}
+
+console.log(arrayPow([0, 5, 2, 7, 3], 2));
+console.log(arrayPow([0, 5, 2, 7, 3], 3));
