6eb9baa0118fac9e85155700941724c297f4a8f2
Yulia Nastenko <yulia.nastenko@gmail.com>
2016-11-06 19:35:12 +0100

    And

--- and.js
+++ and.js
@@ -2,7 +2,6 @@ function and(array1, array2) {
   var result = [];
 
   for (var i = 0; i < array1.length; i++) {
-
     for (var j = 0; j < array2.length; j++) {
       if (array1[i] == array2[j]) {
         result.push(array1[i]);
@@ -12,7 +11,7 @@ function and(array1, array2) {
   return result;
 }
 
-console.log(and([1, 2, 4, 5, 7, 8], [2, 0, 3, 9, 6, 7]));
-console.log(and([1, 5], [4, 6, 7]));
+console.log(and([1, 1, 0, 5, 7, 8], [1, 1, 1, 9, 6, 7]));
+console.log(and([1, 1], [4, 6, 7, 4, 1]));
 console.log(and([1, 2], [2, 7]));
 console.log(and([-7, 0], [-2, 0]));
